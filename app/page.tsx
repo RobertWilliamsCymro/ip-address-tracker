c4331191222f2da2a61fc280b7a28667a489f742
RobertWilliamsCymro <robert.williams@jisc.ac.uk>
2023-09-19 13:47:37 +0100

    add in if statement to api, validate user input, return actual error message and replace flexbox with grid for better layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,45 +5,67 @@ import { useState } from 'react';
 import SearchForm from '@/components/SearchForm';
 
 export default function Home() {
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const fetcher = (url: string) => fetch(url).then((res) => {
+    if (!res.ok) {
+      throw new Error("Bad request");
+    }
+    return res.json();
+  });
 
   const [ipAddress, setIpAddress] = useState("");
+  const checkIpAddress = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+  const checkDomain = /^[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
 
-  const { data, error, } = useSWR(`https://ipwhois.app/json/${ipAddress}`, fetcher);
+  let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.NEXT_PUBLIC_IPIFY_API_KEY}`;
+
+    // use .test() method from regex to check if input is an IP address or domain name
+  if (checkIpAddress.test(ipAddress)) {
+    url += `&ipAddress=${ipAddress}`;
+  } else if (checkDomain.test(ipAddress)) {
+    url += `&domain=${ipAddress}`;
+  }
+  
+  const { data, error } = useSWR(url, fetcher);
 
   const handleSubmit = (ipAddress: string) => {
+    if (ipAddress.includes('/')) {
+      alert('Input cannot contain forward slashes');
+      return;
+    }
     setIpAddress(ipAddress);
   };
 
-  if (error) return <div>Failed to load</div>;
+  if (error) return <div>Error: {error.message}</div>;
   if (!data) return <div>Loading...</div>;
 
   return (
     <main>
-    <div className="bg-no-repeat bg-cover h-72 relative bg-pattern-mobile sm:bg-pattern-desktop">
+      <div className="bg-no-repeat bg-cover h-72 relative bg-pattern-mobile sm:bg-pattern-desktop">
         <h1 className="text-3xl mb-6 pt-10 font-rubik-medium text-white text-center">IP Address Tracker</h1>
         <SearchForm onSubmit={handleSubmit} />
-        <div className="flex flex-col lg:flex-row relative mt-4 lg:mt-8 p-4 lg:p-10 justify-between mx-auto items-center rounded-md bg-white w-8/12 border-black z-[500] text-center lg:text-left">
-          <div className="lg:pr-4 lg:border-r">
-            <p className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">IP Address</p>
-             <p data-testid="ip" className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.ip}</p>
+        <article className="bg-white rounded-lg shadow p-2 md:p-8 mx-10 md:mx-8 grid grid-cols gap-4 mt-4 md:grid-cols-2 lg:grid-cols-4 max-w-6xl xl:mx-auto text-center lg:text-left lg:mt-10 relative"
+        style={{zIndex:10000}}>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">IP Address</h2>
+            <p  className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.ip}</p>
           </div>
-          <div className="lg:px-4 lg:border-r">
-            <p className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">Location</p>
-              <p data-testid="location" className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.city}, {data.country}</p>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">Location</h2>
+            <p className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.location.city}, {data.location.country}</p>
           </div>
-          <div className="lg:px-4 lg:border-r">
-            <p className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">Timezone</p>
-             <p data-testid="timezone" className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.timezone}</p>
+          <div className="lg:border-r lg:border-slate-400">
+            <h2 className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">Timezone</h2>
+            <p className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.location.timezone}</p>
           </div>
-          <div className="lg:pl-4">
-            <p className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">ISP</p>
-             <p className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.isp}</p>
+          <div className="">
+            <h2 className="text-sm sm:text-md font-rubik-medium font- uppercase text-custom-100">ISP</h2>
+            <p className="text-lg lg:text-2xl font-rubik-medium mb-4">{data.isp}</p>
           </div>
-         </div>
+        </article>
       </div>
 
-      <Map latitude={data.latitude} longitude={data.longitude} />
+      <Map latitude={data.location.lat} longitude={data.location.lng} />
     </main>
   )
 }
